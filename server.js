8d749bfb3414413da4599b59d2398e7057fb2641
v.derevianko <v.derevianko@adbakers.com.ua>
2020-08-28 17:06:09 +0300

    add cors

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config({path:'variables.env'});
+// const swaggerJsDoc = require('swagger-jsdoc');
+// const swaggerUi = require('swagger-ui-express');
+var cors = require('cors');
+app.use(cors);
 app.use(bodyParser.json());
 app.use(express.static('./storage/public'));
+
 const productRoutes = require('./routes/productRoutes');
 const sliderRoutes = require('./routes/sliderRoutes');
 const formRoutes = require('./routes/consumerFormRoutes');
@@ -11,10 +16,29 @@ const formRoutes = require('./routes/consumerFormRoutes');
 const handlers = require('./errorHandlers/errHandler')
 require('./conn/mongooseConn');
 
+
+// // Extended https://swagger.io/specification/#infoObject
+// const swaggerOptions = {
+//     swaggerDefinition: {
+//         basePath: '/',
+//         info: {
+//             title: "Shop-API",
+//             description: "Shop endpoints, standard requests & responses",
+//             contact: {
+//                 name: "Dev"
+//             },
+//             servers: ["http://localhost:3000"]
+//         }
+//     }, 
+//     apis: ["server.js"]
+// }
+
+// const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+// app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api/v1/product', productRoutes);
 app.use('/api/v1/slider', sliderRoutes);
 app.use('/api/v1/admin/consumer-form', formRoutes);
-
 app.get('/', (req, res) => {
 res.send('mainpage');
 });
