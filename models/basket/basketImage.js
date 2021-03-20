e2aec60afc900b0d8985bc03716cbfd466066eeb
v.derevianko <4944995@gmail.com>
2021-03-20 15:50:18 +0200

    set origin _id to cart products

--- models/basket/basketImage.js
+++ models/basket/basketImage.js
@@ -1,22 +1,12 @@
-const { Binary } = require('mongodb');
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const iSchema = new mongoose.Schema({
     alt: String,
     url: String,
+    image: String,
     createdAt: String,
-    // baseCode: String
 })
-
-iSchema.pre('save', function(next){
-    const image = this.baseCode
-    next();
-});
-iSchema.pre('update', function(next){
-    const image = this.baseCode
-    next();
-});
 
 exports.iSchema = iSchema;
 exports.imageModel = mongoose.model('BasketImage', iSchema)
