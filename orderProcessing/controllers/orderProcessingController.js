f4082f20188c3bb3bedbeae2dfccf26c1212f5ed
victorderevyanko <4944995@gmail.com>
2022-08-02 18:41:19 +0300

    fix mail credentials

--- orderProcessing/controllers/orderProcessingController.js
+++ orderProcessing/controllers/orderProcessingController.js
@@ -10,19 +10,21 @@ module.exports = class orderProcessing extends  BaseController{
     }
 
     async updateOrder(request){
-        const id = request.param('id')
-        const status = request.param('status')
+        const id = request.param('id');
+        const status = request.param('status');
         try{
-            const date = new Date()
-            const orderObject = await this.model.findOne({_id: id})
+            const date = new Date();
+            const orderObject = await this.model.findOne({_id: id});
             orderObject.processing.push( {content: request.body.content, processingStatus: status, createdAt : date.toISOString()})
-            orderObject.orderStatus = status
-            const order = await orderObject.save()
-            const sendTo = definer(status, orderObject.userInfo.email)
-            event(sendTo, status, {msg: request.body.content}) // will be send only if sentTo differ than undefined
+            orderObject.orderStatus = status;
+            const order = await orderObject.save();
+            const sendTo = definer(status, orderObject.userInfo.email);
+            event(sendTo, status, {msg: request.body.content});
+            // will be send only if sentTo differ than undefined
 
             return {message: this.messages.message.create.success, code: this.accepted, data: order}
         }catch(err){
+            console.log(err);
             return {message: this.messages.message.show.unprocessable, code: this.serverError, data: null}
         }
     }
